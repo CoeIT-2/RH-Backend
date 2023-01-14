8dfa0f38e52855ec59f8d323a9a70036f8b09dba
Fares Mezenner <faresmezenner@gmail.com>
2023-01-14 21:54:33 +0100

    created the models, controllers and routes

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const createError = require('http-errors');
 const morgan = require('morgan');
+const member = require('./src/api/routes/memberRoute')
+const badge = require('./src/api/routes/badgeRoute')
+const task = require('./src/api/routes/taskRoute')
+const notification = require('./src/api/routes/notificationRoute')
 require('dotenv').config();
+const connectDB = require('./src/config/db_init.js')
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
+
+connectDB();
+
+//Main Route
 app.get('/', async (req, res, next) => {
   res.send({ message: 'Awesome it works 🐻' });
 });
@@ -24,6 +33,25 @@ app.use((err, req, res, next) => {
     message: err.message,
   });
 });
+
+
+
+
+//call routes
+app.use('/members', member)
+app.use('/badge', badge)
+app.use('/task', task)
+app.use('/notification', notification)
+
+
+
+// if the route is not found
+app.use('*', (req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
